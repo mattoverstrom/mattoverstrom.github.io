974cf7597bf4f210e323184bb838465defa601df
mattyo <matthewoverstrom@gmail.com>
2020-09-13 23:01:41 -0400

    gitit

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
-    API_KEY : 'AIzaSyAxghYShlWVEjAM3dzr7xCEfKOvLOMuHcI',
+    MY_KEY : 'AIzaSyAxghYShlWVEjAM3dzr7xCEfKOvLOMuHcI',
     SECRET_KEY : '567d89',
     KEY_2 : '101010d'
   }
